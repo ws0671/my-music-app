cf7929abf386f369621101b23a762ee9295def64
ws0671 <ws0671@naver.com>
2024-06-11 19:19:07 +0900

    feat: 깃허브 로그인

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,20 +1,26 @@
 import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 
 const supabase = createClient(
   "https://ayrqfhuebatobjierkyu.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5cnFmaHVlYmF0b2JqaWVya3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3NTQ1MDcsImV4cCI6MjAzMzMzMDUwN30.GysEdNw_lxaOeCoqIm1BVvCoLjpUEldCGZ-bcA2Jc2Q"
 );
 
-const { data, error } = await supabase.auth.signUp({
-  email: "example@email.com",
-  password: "example-password",
-});
 export default function CreateAccount() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const oAuthLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:5173/",
+      },
+    });
+  };
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -26,8 +32,22 @@ export default function CreateAccount() {
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log(email, name, password);
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: "http://localhost:5173/",
+        data: {
+          name,
+        },
+      },
+    });
+
+    if (error) console.log(error);
+    if (error !== null) setError(error.message);
+    console.log(data);
   };
 
   return (
@@ -39,7 +59,10 @@ export default function CreateAccount() {
         <button className="border px-4 py-2 rounded shadow">
           구글로 계속하기
         </button>
-        <button className="border px-4 py-2 rounded shadow">
+        <button
+          onClick={oAuthLogin}
+          className="border px-4 py-2 rounded shadow"
+        >
           깃허브로 계속하기
         </button>
         <button className="border px-4 py-2 rounded shadow">
@@ -50,10 +73,10 @@ export default function CreateAccount() {
             <div className="border-t border-black w-full"></div>
           </div>
           <div className="relative flex justify-center">
-            <span className="bg-white px-2">또는</span>
+            <span className="bg-white text-sm px-2">또는</span>
           </div>
         </div>
-        <Form className="flex flex-col gap-4 w-full" onSubmit={onSubmit}>
+        <form className="flex flex-col gap-4 w-full" onSubmit={onSubmit}>
           <input
             className="px-4 py-2 w-full focus:outline-none bg-gray-100 border-solid border rounded"
             onChange={onChange}
@@ -82,11 +105,12 @@ export default function CreateAccount() {
             required
           />
           <input
-            className="px-4 py-2 text-white bg-orange-300 rounded"
+            className="px-4 py-2 hover:cursor-pointer hover:bg-orange-300 text-white bg-orange-400 rounded"
             type="submit"
             value="가입하기"
           />
-        </Form>
+        </form>
+        {error !== "" ? <h1>{error}</h1> : null}
       </div>
     </div>
   );
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,35 @@
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const supabase = createClient(
+  "https://ayrqfhuebatobjierkyu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5cnFmaHVlYmF0b2JqaWVya3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3NTQ1MDcsImV4cCI6MjAzMzMzMDUwN30.GysEdNw_lxaOeCoqIm1BVvCoLjpUEldCGZ-bcA2Jc2Q"
+);
+
 export default function Home() {
-  return <h1>Home!</h1>;
+  const [loginState, setLoginState] = useState(false);
+  const checkLogin = async () => {
+    const { data, error } = await supabase.auth.getSession();
+    const session = data.session;
+    session !== null ? setLoginState(true) : setLoginState(false);
+    console.log(session);
+  };
+  useEffect(() => {
+    checkLogin();
+  }, [loginState]);
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    setLoginState(false);
+  };
+  return (
+    <>
+      <h1>Home!</h1>
+      {loginState ? (
+        <button onClick={signOut}>logout</button>
+      ) : (
+        <Link to={"create-account"}>signup</Link>
+      )}
+    </>
+  );
 }
