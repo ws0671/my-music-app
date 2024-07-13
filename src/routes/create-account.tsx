3aaa2902ab1ef7e5a07cdac9fc20a916699c4dca
ws0671 <ws0671@naver.com>
2024-07-13 19:49:22 +0900

    fix: redirectTo 수정

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -23,7 +23,7 @@ export default function CreateAccount() {
     await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: "https://ayrqfhuebatobjierkyu.supabase.co/auth/v1/callback",
+        redirectTo: "https://my-music-app-eta.vercel.app/",
       },
     });
   };
